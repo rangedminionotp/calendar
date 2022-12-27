1c4f216ee811ae5c1e61d0995a944d94830028ff
jli511 <jli511@ucsc.edu>
2022-12-27 13:58:26 -0800

    made it more oo

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
+window.addEventListener('DOMContentLoaded', function(e) {
+  const picker = new Picker('picker');
+  picker.setCurrentDate();
+  document.getElementById('prev').addEventListener('click', function(e) {
+    picker.arrowPrev();
+  });
+  document.getElementById('next').addEventListener('click', function(e) {
+    picker.arrowNext();
+  });
+  document.getElementById('display').addEventListener('click', function(e) {
+    picker.setTodayDate();
+  });
+  // https://bobbyhadz.com/blog/javascript-addeventlistener-queryselectorall#:~:text=To%20add%20an%20event%20listener,each%20element%20in%20the%20collection.
+  // how to add event listener to every element with same class
+  const allDays = document.querySelectorAll('.currDay, .nextMonth, .prevMonth');
+  allDays.forEach(day => {
+    day.addEventListener('click', function(e) {
+      if (day.className == 'currDay') {
+        picker.currDayChange(e.target);
+      } else if (day.className == 'prevMonth') {
+        picker.prevGreyClick(e.target);
+      } else if (day.className == 'nextMonth') {
+        picker.nextGreyClick(e.target);
+      }
+    });
+  });
+});
+
 const week = ['S', 'M', 'T', 'W', 'T', 'F', 'S'];
 
 const month = [
@@ -15,8 +43,6 @@ const month = [
   'December',
 ];
 
-const todayG = [-1, -1, -1];
-
 const monthlyPics = [
   'https://media.discordapp.net/attachments/368534832197664769/1057327577875882044/january.jpeg',
   'https://bridgesnurseryschool.com/wp-content/uploads/2019/02/screen-shot-2018-01-25-at-9-54-10-am-1516892078-1.png',
@@ -32,38 +58,6 @@ const monthlyPics = [
   'https://media.discordapp.net/attachments/368534832197664769/1057344654720368741/december.jpg'
 ];
 
-window.addEventListener('DOMContentLoaded', function(e) {
-  const picker = new Picker('picker');
-  var values = picker.setCurrentDate();
-  document.getElementById('prev').addEventListener('click', function(e) {
-    values = picker.arrowPrev(values);
-  });
-  document.getElementById('next').addEventListener('click', function(e) {
-    values = picker.arrowNext(values);
-  });
-  document.getElementById('display').addEventListener('click', function(e) {
-    picker.clearToday(values);
-    values = picker.setTodayDate(values);
-  });
-  // https://bobbyhadz.com/blog/javascript-addeventlistener-queryselectorall#:~:text=To%20add%20an%20event%20listener,each%20element%20in%20the%20collection.
-  // how to add event listener to every element with same class
-  const allDays = document.querySelectorAll('.currDay, .nextMonth, .prevMonth');
-  allDays.forEach(day => {
-    day.addEventListener('click', function(e) {
-      if (day.className == 'currDay') {
-        values = picker.currDayChange(e.target, values);
-      } else if (day.className == 'prevMonth') {
-        values = picker.prevGreyClick(e.target, values);
-      } else if (day.className == 'nextMonth') {
-        values = picker.nextGreyClick(e.target, values);
-      }
-    });
-  });
-});
-
-/**
- * CSE186 Assignment 3 - Advanced
- */
 class Picker {
   /**
    * Create a date picker
@@ -71,6 +65,12 @@ class Picker {
   */
   constructor(containerId) {
     this.containerId = containerId;
+    const curr = new Date();
+    this.dd = curr.getDate();
+    this.mm = curr.getMonth();
+    this.yyyy = curr.getFullYear();
+    this.todayG = [this.dd, this.mm, this.yyyy];
+    this.today = -1;
     // append picker to something...
     // create weekday column
     for (let i = 0; i < 7; i++) {
@@ -93,22 +93,17 @@ class Picker {
   /**
    * set date function, like the calender page
    * with prevMonth, currMonth, nextMonth
-   * @param {string} dd date
-   * @param {string} mm month
-   * @param {string} yyyy year
-   * @param {number} today index location of today
-   * @return {string} return dd, mm, yyyy, today, flag
   */
-  setDate(dd, mm, yyyy, today) {
+  setDate() {
     // modify display
     // this.clearToday(today);
-    document.getElementById('currentMonth').innerText = month[mm];
-    document.getElementById('currentYear').innerText = yyyy;
+    document.getElementById('currentMonth').innerText = month[this.mm];
+    document.getElementById('currentYear').innerText = this.yyyy;
     // https://bobbyhadz.com/blog/javascript-get-number-of-days-in-month
     // how to get number of days in month
-    const curMMdays = new Date(yyyy, mm+1, 0).getDate();
+    const curMMdays = new Date(this.yyyy, this.mm+1, 0).getDate();
     // https://bobbyhadz.com/blog/javascript-get-first-day-of-month#:~:text=To%20get%20the%20first%20day,first%20day%20of%20the%20month.
-    const firstDay = new Date(yyyy, mm, 1).getDay();
+    const firstDay = new Date(this.yyyy, this.mm, 1).getDay();
     // https://bobbyhadz.com/blog/javascript-get-last-day-of-month
     // const lastDay = new Date(yyyy, mm+1, 0).getDay();
     // var lastDay = new Date(yyyy, mm+1, 0);
@@ -116,7 +111,7 @@ class Picker {
     // mm = 1
     // last month = 12
     // next month = 2
-    const lastMMdays = new Date(yyyy, mm, 0).getDate();
+    const lastMMdays = new Date(this.yyyy, this.mm, 0).getDate();
     // const nextMMdays = new Date(yyyy, mm+2, 0).getDate();
     // set current month days
     let j = 1;
@@ -126,9 +121,9 @@ class Picker {
       element.innerText = j;
       // element.classList.remove('currDay', 'prevMonth', 'nextMonth');
       element.className = 'currDay';
-      if (j == todayG[0] && mm == todayG[1] && yyyy == todayG[2]) {
+      if (j == this.todayG[0] && this.mm == this.todayG[1] && this.yyyy == this.todayG[2]) {
         element.id = 'today';
-        today = i;
+        this.today = i;
       }
       j++;
     }
@@ -153,8 +148,7 @@ class Picker {
       j++;
     }
     const bgPicker = document.getElementById('picker');
-    bgPicker.style.backgroundImage = `url('${monthlyPics[mm]}')`;
-    return [dd, mm, yyyy, today];
+    bgPicker.style.backgroundImage = `url('${monthlyPics[this.mm]}')`;
   }
   /**
    *
@@ -163,42 +157,32 @@ class Picker {
   setCurrentDate() {
     // https://stackoverflow.com/questions/1531093/how-do-i-get-the-current-date-in-javascript
     // how to get current date
-    const curr = new Date();
-    const dd = curr.getDate();
-    const mm = curr.getMonth();
-    const yyyy = curr.getFullYear();
-    todayG[0] = dd;
-    todayG[1] = mm;
-    todayG[2] = yyyy;
-    const values = this.setDate(dd, mm, yyyy, -1);
-    return values;
+    this.setDate();
   }
 
   /**
    *
    * @param {string} values dd, mm, yyyy, today, flag
    */
-  clearToday(values) {
+  clearToday() {
     const element = document.getElementById('today');
     if (element) {
-      element.id = 'd'+values[3];
+      element.id = 'd'+this.today;
     }
   }
 
   /**
    *
-   * @param {string} values dd, mm, yyyy, today, flag
-   * @return {string} dd, mm, yyyy, today, flag
    */
-  arrowPrev(values) {
+  arrowPrev() {
     // document.write(values[0] , values[1] , values[2]);
-    if (values[1] == 0) {
-      values[2]--;
-      values[1] = 12;
+    if (this.mm == 0) {
+      this.yyyy--;
+      this.mm = 12;
     }
-    this.clearToday(values);
-    const value = this.setDate(values[0], values[1]-1, values[2], values[3]);
-    return value;
+    this.mm--;
+    this.clearToday();
+    this.setDate();
   }
 
   /**
@@ -206,14 +190,14 @@ class Picker {
    * @param {string} values dd, mm, yyyy, today, flag
    * @return {string} dd, mm, yyyy, today, flag
    */
-  arrowNext(values) {
-    if (values[1] == 11) {
-      values[2]++;
-      values[1] = -1;
+  arrowNext() {
+    if (this.mm == 11) {
+      this.yyyy++;
+      this.mm = -1;
     }
-    this.clearToday(values);
-    const value = this.setDate(values[0], values[1]+1, values[2], values[3]);
-    return value;
+    this.mm++;
+    this.clearToday();
+    this.setDate();
   }
 
   /**
@@ -222,22 +206,21 @@ class Picker {
    * @param {string} values dd, mm, yyyy, today, flag
    * @return {string} dd, mm, yyyy, today, flag
    */
-  prevGreyClick(prev, values) {
+  prevGreyClick(prev) {
     // document.write('hi');
     // document.write(prev.textContent);
     // document.write(values[0] , values[1] , values[2]);
-    this.clearToday(values);
-    if (values[1] == 0) {
-      values[2]--;
-      values[1] = 12;
+    this.clearToday();
+    if (this.mm == 0) {
+      this.yyyy--;
+      this.mm = 12;
     }
-    values[1]--;
+    this.mm--;
     const content = prev.textContent;
-    todayG[0] = content;
-    todayG[1] = values[1];
-    todayG[2] = values[2];
-    const value = this.setDate(content, values[1], values[2], values[3]);
-    return value;
+    this.todayG[0] = this.dd = content;
+    this.todayG[1] = this.mm;
+    this.todayG[2] = this.yyyy;
+    this.setDate();
   }
 
   /**
@@ -246,19 +229,18 @@ class Picker {
    * @param {string} values dd, mm, yyyy, today, flag
    * @return {string}  dd, mm, yyyy, today, flag
    */
-  nextGreyClick(next, values) {
-    this.clearToday(values);
-    if (values[1] == 11) {
-      values[2]++;
-      values[1] = -1;
+  nextGreyClick(next) {
+    this.clearToday();
+    if (this.mm == 11) {
+      this.yyyy++;
+      this.mm = -1;
     }
-    values[1]++;
+    this.mm++;
     const content = next.textContent;
-    todayG[0] = content;
-    todayG[1] = values[1];
-    todayG[2] = values[2];
-    const value = this.setDate(content, values[1], values[2], values[3]);
-    return value;
+    this.todayG[0] = this.dd = content;
+    this.todayG[1] = this.mm;
+    this.todayG[2] = this.yyyy;
+    this.setDate();
   }
 
   /**
@@ -267,14 +249,14 @@ class Picker {
    * @param {string} values dd, mm, yyyy, today, flag
    * @return {string} dd, mm, yyyy, today, flag
    */
-  currDayChange(curr, values) {
-    this.clearToday(values);
+  currDayChange(curr) {
+    this.clearToday();
     const content = curr.textContent;
-    todayG[0] = content;
-    todayG[1] = values[1];
-    todayG[2] = values[2];
-    const value = this.setDate(content, values[1], values[2], values[3]);
-    return value;
+    this.todayG[0] = this.dd = content;
+    this.todayG[1] = this.mm;
+    this.todayG[2] = this.yyyy;
+    
+    this.setDate();
   }
 
   /**
@@ -282,10 +264,12 @@ class Picker {
    * @param {string} values dd, mm, yyyy, today, flag
    * @return {string} dd, mm, yyyy, today, flag
    */
-  setTodayDate(values) {
-    this.clearToday(values);
-    const value = this.setDate(todayG[0], todayG[1], todayG[2], values[3]);
-    return value;
+  setTodayDate() {
+    this.clearToday();
+    this.dd = this.todayG[0];
+    this.mm = this.todayG[1];
+    this.yyyy = this.todayG[2];
+    this.setDate();
   }
 }
 // To satisfy linter rules
